1b5d0bbcd9c3c0c9fc23a0dc10f59a9a06dfdef1
ntziris <ntziris@gmail.com>
2021-04-23 13:40:13 +0300

    added dotevn and mongoose packages

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Set up the server
 
 const app = express();
-
 // select the port for heroku or 5000 for local development
 const PORT = process.env.PORT || 5000; 
-
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-
 app.get("/test", (req, res) => {
   res.send("It works");
 });
+
+// Connect to mongoDB
+
+mongoose.connect(process.env.MDB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  err => {
+    if (err) return console.error(err);
+    console.log('Connected to MongoDB');
+  }
+);
+
