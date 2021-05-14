53fd58d199db744a5c4b78575e8ffb94fe72379b
ktrip96 <ktrip96@gmail.com>
2021-05-14 08:26:25 +0300

    Authorization

--- server/index.js
+++ server/index.js
@@ -1,34 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
 // Set up the server
 
-const app = express();
+const app = express()
 // select the port for heroku or 5000 for local development
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-app.get("/test", (req, res) => {
-  res.send("It works");
-});
-app.use(express.json());
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+app.get('/test', (req, res) => {
+  res.send('It works')
+})
+
+// applied middlware for every request
+// converts text request => json request
+app.use(express.json())
 
 // Connect to mongoDB
 
-mongoose.connect(process.env.MDB_CONNECT,
+mongoose.connect(
+  process.env.MDB_CONNECT,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  err => {
-    if (err) return console.error(err);
-    console.log('Connected to MongoDB');
+  (err) => {
+    if (err) return console.error(err)
+    console.log('Connected to MongoDB')
   }
-);
+)
 
 // Set up routes
 
-app.use("/auth", require("./routers/userRouter"));
-
+app.use('/auth', require('./routers/userRouter'))
