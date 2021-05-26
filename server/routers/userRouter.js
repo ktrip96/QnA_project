8d3b5770395a44f747f9fcd9fa114dc3cdd4751b
ktrip96 <ktrip96@gmail.com>
2021-05-26 13:06:09 +0300

    Login Funtionality

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -53,19 +53,65 @@ router.post('/', async (req, res) => {
       process.env.JWT_SECRET
     )
 
-    console.log({ token })
-
     // send the token in a HTTP only cookie
 
     res
       .cookie('token', token, {
         httpOnly: true,
       })
-      .send('Congrats')
+      .send()
   } catch (err) {
     console.error(err)
     res.status(500).send()
   }
 })
 
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ errorMessage: 'Please enter all required fields.' })
+
+    // check if the user exists
+
+    const existingUser = await User.findOne({ email })
+
+    // if not return 401
+
+    if (!existingUser)
+      return res
+        .status(401)
+        .json({ errorMessage: 'Wrong username or password' })
+
+    // Else, compare the given password with the stored hashed password
+    const passwordCorrect = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    )
+
+    if (!passwordCorrect)
+      return res
+        .status(401)
+        .json({ errorMessage: 'Wrong username or password' })
+
+    const token = jwt.sign(
+      {
+        user: existingUser._id,
+      },
+      process.env.JWT_SECRET
+    )
+
+    res
+      .cookie('token', token, {
+        httpOnly: true,
+      })
+      .send()
+  } catch (err) {
+    console.error(err)
+    res.status(500).send()
+  }
+})
 module.exports = router
