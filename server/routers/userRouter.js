8d58af9cfa3aa5ae6b5707cda45d15b18e59cb8b
ktrip96 <ktrip96@gmail.com>
2021-05-26 13:10:31 +0300

    Logout functionality

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -114,4 +114,13 @@ router.post('/login', async (req, res) => {
     res.status(500).send()
   }
 })
+
+router.post('/logout', (req, res) => {
+  res
+    .cookie('token', '', {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    .send()
+})
 module.exports = router
