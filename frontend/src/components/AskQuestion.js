0837ada10731c83b34bac0ddb563c80de366d6f6
ktrip96 <ktrip96@gmail.com>
2021-07-19 17:20:16 +0300

    Ask a Question page incoming

---
+++ frontend/src/components/AskQuestion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AskQuestion() {
+  return (
+    <div>
+      <input>Title</input>
+      <input label='Type your question'></input>
+      <input label='enter tags for your question'></input>
+    </div>
+  )
+}
