a144ebe4f7f72a22c776bc9838378b20dd226bfb
ktrip96 <ktrip96@gmail.com>
2021-07-26 12:20:09 +0300

    Sign Up + Log In Styling

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,12 +2,50 @@ import React, { useState, useContext } from 'react'
 import axios from 'axios'
 import { useHistory } from 'react-router'
 import AuthContext from '../context/AuthContext'
+import styled from 'styled-components'
+import { Input, Button } from '@chakra-ui/react'
+
+const SignUpWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const SignUpBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e2e8f0;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 30px;
+  border-radius: 30px;
+`
+const Title = styled.h1`
+  font-size: 2em;
+  text-align: center;
+  margin-bottom: 1em;
+`
+
+const LogInText = styled.p`
+  font-size: 0.8em;
+  color: #a0aec0;
+  width: 200px;
+  margin-left: auto;
+  margin-bottom: 20px;
+  :hover {
+    cursor: pointer;
+    color: #2d3748;
+    transition: transform 0.2s;
+    transform: scale(1.06);
+  }
+`
 
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [username, setUsername] = useState('')
   const [passwordVerify, setPasswordVerify] = useState('')
+  const [isLoading, setIsLoading] = useState(false)
 
   const history = useHistory()
   const { getLoggedIn } = useContext(AuthContext)
@@ -21,45 +59,70 @@ export default function Signup() {
         username,
         passwordVerify,
       }
-      console.log(registerData)
+      setIsLoading(true)
       await axios.post('http://localhost:5000/auth/', registerData)
       await getLoggedIn()
       history.push('/')
+      setIsLoading(false)
     } catch (err) {
       console.error(err.response)
     }
   }
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input
+    <SignUpWrapper>
+      <SignUpBox>
+        <Title>Sign Up</Title>
+
+        <Input
           type='email'
           placeholder='Type your email'
           onChange={(e) => setEmail(e.target.value)}
           value={email}
+          mb={6}
         />
-        <input
+        <Input
           type='username'
           placeholder='Type your username'
           onChange={(e) => setUsername(e.target.value)}
           value={username}
+          mb={6}
         />
-        <input
+        <Input
           type='password'
           placeholder='Type your password'
           onChange={(e) => setPassword(e.target.value)}
           value={password}
+          mb={6}
         />
-        <input
+        <Input
           type='password'
           placeholder='Verify your password'
           onChange={(e) => setPasswordVerify(e.target.value)}
           value={passwordVerify}
+          mb={6}
         />
-        <button>Sign up</button>
-      </form>
-    </div>
+        <LogInText onClick={() => history.push('/login')}>
+          Already have an account? Sign in
+        </LogInText>
+        {isLoading ? (
+          <Button
+            colorScheme='teal'
+            isLoading
+            onClick={handleSubmit}
+            size='md'
+          ></Button>
+        ) : (
+          <Button
+            colorScheme='teal'
+            isLoading={false}
+            onClick={handleSubmit}
+            size='md'
+          >
+            Sign Up
+          </Button>
+        )}
+      </SignUpBox>
+    </SignUpWrapper>
   )
 }
