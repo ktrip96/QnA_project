a91eae6a047458e738b62676396c88b72d3524ea
ktrip96 <ktrip96@gmail.com>
2021-07-31 23:30:52 +0300

    heroku commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,21 +1,22 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 function auth(req, res, next) {
   try {
-    const token = req.cookies.token;
+    const token = req.cookies.token
 
-    // If you don't have token, then you are not authorized to do the API call
+    // If you don't have token, then you are not authorized to do the API call\
+
     if (!token)
-      return res.status(401).json({ success: 0, message: "Unauthorized" });
+      return res.status(401).json({ success: 0, message: 'Unauthorized' })
 
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified.user;
+    const verified = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = verified.user
 
-    next();
+    next()
   } catch (err) {
-    console.error(err);
-    res.status(401).json({ success: 0, message: "Unauthorized" });
+    console.error(err)
+    res.status(401).json({ success: 0, message: 'Unauthorized' })
   }
 }
 
-module.exports = auth;
+module.exports = auth
